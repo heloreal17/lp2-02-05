ea6776e4e2fb220702f988e64542e12065ead965
Heloísa Real <85493260+heloreal17@users.noreply.github.com>
2022-05-04 12:13:23 -0300

    Add files via upload

--- routes/gallery.js
+++ routes/gallery.js
@@ -38,4 +38,35 @@ module.exports = (app)=>{
             }
         })
     })
+
+    //visualizar a imagem selecionada
+    app.get('/alterar_gallery', async(req,res)=>{
+        //recuperar o parâmetro id da barra de endereço
+        var id = req.query.id
+        //procurar por um documento com o id
+        var busca = await gallery.findOne({_id:id})
+        //abrir o arquivo gallery_alterar
+        res.render('gallery_alterar.ejs', {dados:busca})
+    })
+
+
+    //alterar a imagem gravada no documento
+    app.post('/alterar_gallery',(req,res)=>{
+        //upload  das imagens
+        upload(req,res,async (err)=>{
+            if(err instanceof multer.MulterError){
+                res.send("O arquivo é maior que 100kb")
+            }else if(err){
+                res.send('Tipo de Arquivo inválido')
+            }else{
+                //conectar ao database
+                database()
+                //alterar o nome do arquivo na coleção gallery
+                var documento = await gallery.findOneAndUpdate({_id:req.query.id},{
+                arquivo:req.file.filename})
+                res.redirect('/gallery')
+
+            }   
+        })
+    })
 }
